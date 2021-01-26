945dd393bb9e0ef3cd55d90f54565573e31853c1
txd22081999 <txd22081999@gmail.com>
2021-01-26 16:41:09 +0700

    update button

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 window.onload = () => {
-  console.log('asdasd');
-  const name = document.querySelector('#name');
-  const password = document.querySelector('#password');
-  const leftEye = document.querySelector('#LeftEye');
-  const rightEye = document.querySelector('#RightEye');
-  const svg = document.querySelector('svg');
-  const smile = document.querySelector('#SmileLip');
+  console.log("asdasd");
+  const form = document.querySelector(".form");
+  const name = document.querySelector("#name");
+  const password = document.querySelector("#password");
+  const leftEye = document.querySelector("#LeftEye");
+  const rightEye = document.querySelector("#RightEye");
+  const svg = document.querySelector("svg");
+  const smile = document.querySelector("#SmileLip");
 
   console.log(smile);
 
   const MAX_LENGTH = 22;
   const MAX_OFFSET = 8;
 
-  name.addEventListener('input', (e) => {
+  console.log(form);
+
+  name.addEventListener("input", (e) => {
     const value = e.target.value;
     // console.log(value.length);
     const percent = Math.floor((value.length / MAX_LENGTH) * 100);
@@ -21,7 +24,7 @@ window.onload = () => {
     let x = offset - 8;
     const y = 6;
     console.log(percent);
-    smile.style = 'opacity: 0';
+    smile.style = "opacity: 0";
 
     // let sign;
     // if (value.length < MAX_LENGTH / 2) {
@@ -40,21 +43,25 @@ window.onload = () => {
 
     leftEye.style = `transform: translate(${x}px, ${y}px)`;
     rightEye.style = `transform: translate(${x}px, ${y}px)`;
+    // form.classList.remove("idle");
   });
 
-  name.addEventListener('blur', () => {
+  name.addEventListener("blur", () => {
     leftEye.style = `transform: translate(0px, 0px)`;
     rightEye.style = `transform: translate(0px, 0px)`;
+    // form.classList.add("idle");
   });
 
-  password.addEventListener('focus', (e) => {
+  password.addEventListener("focus", (e) => {
     // console.log(e.target.value);
-    svg.classList.add('up');
-    smile.style = 'opacity: 1';
+    svg.classList.add("up");
+    smile.style = "opacity: 1";
+    // form.classList.remove("idle");
   });
 
-  password.addEventListener('blur', (e) => {
+  password.addEventListener("blur", (e) => {
     // console.log(e.target.value);
-    svg.classList.remove('up');
+    svg.classList.remove("up");
+    // form.classList.add("idle");
   });
 };
